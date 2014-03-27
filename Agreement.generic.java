d7ce806c4dc91cd1a30915ff84a4ec7b2c63b805
Martin Tornqvist <martin.tornqvist@free.fr>
2014-03-27 10:13:52 +0100

    Update Agreement.generic.java

--- Agreement.generic.java
+++ Agreement.generic.java
@@ -1,28 +1,38 @@
-
 import java.util.Date;
+    
+public class Main {
 
-class Agreement { 
-  String version;           // Version of Agreement 
-  String author;            // Author of Agreement
-  Date signingDate;         // Date of Signing
-  Date effectiveDate;       // Date Agreement is Effective
+    public static void main(String[] args) {
+
+        System.out.println(Agreement);}
+
+    public class Agreement { 
+      String version;           // Version of Agreement 
+      String author;            // Author of Agreement
+      Date signingDate;         // Date of Signing
+      Date effectiveDate;       // Date Agreement is Effective
   
-  String[] parties[];       // Parties to the Agreement  
+      String[] parties[];       // Parties to the Agreement  
   
-  String preamble;
-  String conventions;
-  String agreements;
-  String representations;
-  String warranties;
-  String covenants;
-  String termination;
-  String defaultevent;
-  String close_out;
-  String misc;
-  String law;
-  String courts;
+      String preamble = "TEST";
+      String conventions;
+      String agreements;
+      String representations;
+      String warranties;
+      String covenants;
+      String termination;
+      String defaultevent;
+      String close_out;
+      String misc;
+      String law;
+      String courts;
   
-  String[] signatures[];
+      String[] signatures[];
   
-  String[] Schedule[];
-  }
+      String[] Schedule[];
+  
+  }}
+
+  
+
+  }}
