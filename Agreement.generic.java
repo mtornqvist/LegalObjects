25a2e46572f81e655e59ad492093e49135a6b6b6
Martin Tornqvist <martin.tornqvist@free.fr>
2014-04-07 23:46:01 +0200

    Update Agreement.generic.java

--- Agreement.generic.java
+++ Agreement.generic.java
@@ -2,32 +2,47 @@ import java.util.Date;
 import java.util.Scanner;
 
 class Agreement { 
-    int i;
+    int i, np;
     String name;                // Name of Agreement
     String version;             // Version of Agreement 
     String author;              // Author of Agreement
     Date signingDate;           // Date of Signing
     Date effectiveDate;         // Date Agreement is Effective
   
-    String parties[];           // Parties to the Agreement  
+    Party parties[];           // Parties to the Agreement  
   
-    String sections[];
     Section s[] = new Section[20];
+    String sections[];
     
     String[] signatures[];
     String[] schedule[];
     
-    Agreement (String n, String v, String sections[]){
+    Agreement (String n, String v, int np, String sections[]){
         int i;
         name = n;
         version = v;
-                      
+        
+        Party parties[] = new Party[np];
+        
+        for (i = 0; i < np; i++)
+            parties[i] = new Party();
+         
         for (i = 0; i < sections.length; i++)
             s[i] = new Section(sections[i]);
     }
 }
 
- class Section {
+class Party {
+    String name;
+    String address;
+    String lei;
+    String signatory;
+    String signatoryTitle;
+    
+    Party () {}
+} 
+
+class Section {
     static int number = 0;
     String heading;
     
@@ -58,9 +73,13 @@ class LegalObjects {
         int i;
         String sections[] = {"Preamble", "Agreements", "Representations", "Warranties", "Covenants", "Termination", "Events of default", "Close out", "Misc", "Law", "Courts"}; 
                 
-        System.out.print( "Type the name of the Agreement: " );
-        String input = scanner.nextLine();
-        Agreement a = new Agreement(input, "01", sections);
+        System.out.print( "Name of the Agreement: " );
+        String inputName = scanner.nextLine();
+        
+        System.out.print( "Number of Parties to Agreement: " );
+        int inputNoParties = scanner.nextInt();
+        
+        Agreement a = new Agreement(inputName, "01", inputNoParties, sections);
         
         System.out.println(a.name);
         System.out.println(a.version);
